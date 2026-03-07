18a863c279a30b668ab24e71bc254dc3a843ceee
ydlee <devlee8585@gmail.com>
2026-03-07 19:01:24 +0900

    rust chapter 1

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-098401bf122644dc.d
@@ -0,0 +1,5 @@
+C:\workspace\study\rust\projects\hello_cargo\target\debug\deps\hello_cargo-098401bf122644dc.d: src\main.rs
+
+C:\workspace\study\rust\projects\hello_cargo\target\debug\deps\libhello_cargo-098401bf122644dc.rmeta: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\workspace\study\rust\projects\hello_cargo\target\debug\hello_cargo.exe: C:\workspace\study\rust\projects\hello_cargo\src\main.rs
